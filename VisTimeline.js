00e07e183f57eaa5c3ee613daaff1696f9b4a447
thegsi <dr.gethin.rees@gmail.com>
2019-09-26 08:52:47 +0100

    add histogram for Georeferencer/EAP

--- VisTimeline.js
+++ VisTimeline.js
@@ -1,4 +1,4 @@
-function createVisTimeline(data){
+function createVisTimeline(data, visualisation){
 
   // item = {
   //   id: i,
@@ -7,45 +7,118 @@ function createVisTimeline(data){
   //   content: ''
   // };
 
-  var items = new vis.DataSet();
-  var dates = []
-  data.features.forEach(function(feature, i){
-    var item = {};
+  if (visualisation === 'horizontal') {
+    var items = new vis.DataSet();
+    var dates = []
 
-    if (feature.properties.when && feature.properties.when.timespans.length) {
-       var spans = feature.properties.when.timespans
-       if (spans[0].start.in) item['start'] = new Date().setYear(spans[0].start.in)
-       if (spans[0].end.in) item['end'] = new Date().setYear(spans[0].end.in)
-       if (spans[0].start.earliest) item['start'] = new Date().setYear(spans[0].start.earliest)
-       if (spans[0].end.latest) item['end'] = new Date().setYear(spans[0].end.latest)
+    data.features.forEach(function(feature, i){
+      var item = {};
+
+      if (feature.properties.when && feature.properties.when.timespans.length) {
+         var spans = feature.properties.when.timespans
+         if (spans[0].start.in) item['start'] = new Date().setYear(spans[0].start.in)
+         if (spans[0].end.in) item['end'] = new Date().setYear(spans[0].end.in)
+         if (spans[0].start.earliest) item['start'] = new Date().setYear(spans[0].start.earliest)
+         if (spans[0].end.latest) item['end'] = new Date().setYear(spans[0].end.latest)
+         if (typeof spans[0].start === 'string') item['start'] = new Date().setYear(spans[0].start)
+         if (typeof spans[0].end === 'string') item['end'] = new Date().setYear(spans[0].end)
+      }
+
+      item['id'] = feature.properties.name + i
+      item['content'] = feature.properties.name + i
+
+      dates.push(item['end'])
+      dates.push(item['start'])
+      items.add(item);
+    });
+
+    dates.sort(function(a, b){return a-b});
+    var options = {
+      stack: true,
+      horizontalScroll: true,
+      zoomKey: 'ctrlKey',
+      maxHeight: 400,
+      start: new Date(dates[0]),
+      end: new Date(dates[dates.length - 1]),
+      editable: true,
+      margin: {
+        item: 10, // minimal margin between items
+        axis: 5   // minimal margin between items and the axis
+      },
+      orientation: 'top'
+    };
+
+    var container = document.getElementById('vis-timeline');
+    timeline = new vis.Timeline(container, items, options);
+  }
+
+  if (visualisation === 'histogram') {
+    // {x: '2014-06-16', y: 30}
+    bin = {}
+
+    data.features.forEach(function(feature, i){
+
+      item = {}
+      if (feature.properties.when && feature.properties.when.timespans.length) {
+         var spans = feature.properties.when.timespans
+         if (spans[0].start) {
+           formatDate = moment(spans[0].start).format('YYYY-MM-DD');
+           if (!bin[formatDate]) {
+             bin[formatDate] = 1
+           } else {
+             bin[formatDate] += 1
+           }
+           item['start'] = new Date().setYear(spans[0].start)
+         }
+         // if (spans[0].end) item['end'] = new Date().setYear(spans[0].start.in)
+         // if (spans[0].end) item['start'] = new Date().setYear(spans[0].start.in)
+         // if (spans[0].start.in) item['start'] = new Date().setYear(spans[0].start.in)
+         // if (spans[0].end.in) item['end'] = new Date().setYear(spans[0].end.in)
+         // if (spans[0].start.earliest) item['start'] = new Date().setYear(spans[0].start.earliest)
+         // if (spans[0].end.latest) item['end'] = new Date().setYear(spans[0].end.latest)
+      }
+
+      // item['id'] = feature.properties.title + i
+      // item['content'] = feature.properties.title + i
+      //
+      // dates.push(item['end'])
+      // dates.push(item['start'])
+      // items.add(item);
+    })
+
+    var items = Object.keys(bin).map(function(date, i) {
+      return {
+        x: date,
+        y: bin[date]
+      }
+    });
+
+    function sortByKey(array, key) {
+      return array.sort(function(a, b) {
+          var x = moment(a[key]).format('YYYY');
+          var y = moment(b[key]).format('YYYY');
+          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      });
     }
 
-    item['id'] = feature.properties.name + i
-    item['content'] = feature.properties.name + i
+    var sortItems = sortByKey(items, 'x')
 
-    dates.push(item['end'])
-    dates.push(item['start'])
-    items.add(item);
-  });
+    var options = {
+        style:'bar',
+        barChart: {width:50, align:'center'}, // align: left, center, right
+        drawPoints: false,
+        maxHeight: 400,
+        dataAxis: {
+            icons:true
+        },
+        orientation:'top',
+        start: items[0].x,
+        end: items[items.length - 1].x
+    };
 
-  dates.sort(function(a, b){return a-b});
+    var container = document.getElementById('vis-timeline');
+    timeline = new vis.Graph2d(container, items, options);
+  }
 
-  var options = {
-    stack: true,
-    horizontalScroll: true,
-    zoomKey: 'ctrlKey',
-    maxHeight: 400,
-    start: new Date(dates[0]),
-    end: new Date(dates[dates.length - 1]),
-    editable: true,
-    margin: {
-      item: 10, // minimal margin between items
-      axis: 5   // minimal margin between items and the axis
-    },
-    orientation: 'top'
-  };
 
-  // create a Timeline
-  var container = document.getElementById('vis-timeline');
-  timeline = new vis.Timeline(container, items, options);
 }
